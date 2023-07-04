657b20aa16255de5b6a157bb16b0361f1abd7a29
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-07-04 23:52:15 +0200

    123

--- host/src/App.vue
+++ host/src/App.vue
@@ -3,9 +3,27 @@
     <div>Name: host</div>
     <div>Framework: vue3</div>
     <Counter />
+    <div class="bg-sky-600">
+      <input type="file" @change="handleFileChange" />
+      <p>Archivo seleccionado: {{ selectedFile ? selectedFile.name : 'Ningún archivo seleccionado' }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Counter from "remote/Counter";
+import ref from "vue";
+
+const fileInput = ref();
+const handleFileChange = (event) => {
+  const file = event.target.files[0]; // Obtiene el primer archivo seleccionado
+
+  if (file) {
+    this.selectedFile = file; // Asigna el archivo seleccionado a la variable
+    // Aquí puedes realizar cualquier acción adicional con el archivo seleccionado, como enviarlo a una API, procesarlo, etc.
+  } else {
+    this.selectedFile = null; // Si no se selecciona ningún archivo, establece la variable en null
+  }
+};
+
+// import Counter from "remote/Counter";
 </script>
